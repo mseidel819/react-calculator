0c3c0ca5040a0107a92286f2e2815ef6e04fdcf2
mseidel819 <mseidel819@gmail.com>
2022-02-13 21:20:40 -0600

    initial commit for github pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,112 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+  const [calc, setCalc] = useState("");
+  const [result, setResult] = useState("");
+
+  const operators = ["/", "*", "+", "-", "."];
+  const updateCalc = (value) => {
+    if (operators.includes(value) && calc === "") return;
+
+    if (operators.includes(value) && operators.includes(calc.slice(-1))) {
+      const valup = calc.slice(0, -1);
+
+      setCalc(valup + value);
+      return;
+    }
+
+    setCalc(calc + value);
+
+    if (!operators.includes(value)) {
+      setResult(eval(calc + value).toString());
+      console.log(calc + value);
+    }
+  };
+
+  const numsButt = () => {
+    const digits = [];
+    const ids = [
+      "one",
+      "two",
+      "three",
+      "four",
+      "five",
+      "six",
+      "seven",
+      "eight",
+      "nine",
+    ];
+    for (let i = 1; i < 10; i++) {
+      digits.push(
+        <button
+          onClick={() => updateCalc(i.toString())}
+          id={ids[i - 1]}
+          key={i}
         >
-          Learn React
-        </a>
-      </header>
+          {i}
+        </button>
+      );
+    }
+    return digits;
+  };
+
+  const calculate = () => {
+    setCalc(eval(calc).toString());
+  };
+
+  const deleteLast = () => {
+    if (calc === "") {
+      return;
+    }
+
+    const value = calc.slice(0, -1);
+    setCalc(value);
+  };
+
+  const clear = () => {
+    setCalc("");
+  };
+
+  return (
+    <div className="app">
+      <div className="calculator">
+        <div id="display" className="display">
+          {calc || 0}
+        </div>
+        <div className="operators">
+          <button id="divide" onClick={() => updateCalc("/")}>
+            /
+          </button>
+          <button id="multiply" onClick={() => updateCalc("*")}>
+            X
+          </button>
+          <button id="add" onClick={() => updateCalc("+")}>
+            +
+          </button>
+          <button id="subtract" onClick={() => updateCalc("-")}>
+            -
+          </button>
+
+          <button onClick={deleteLast}>DEL</button>
+          <button id="clear" onClick={clear}>
+            AC
+          </button>
+        </div>
+        <div className="numbers">
+          {numsButt()}
+          <button id="zero" onClick={calc ? () => updateCalc("0") : clear}>
+            0
+          </button>
+          <button id="decimal" onClick={() => updateCalc(".")}>
+            .
+          </button>
+
+          <button id="equals" onClick={calculate}>
+            =
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
